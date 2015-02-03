93f127b3ccefdb8d5d9b25af7634276ceec39d3f
tecosner1425 <tecosner1425@gmail.com>
2015-02-03 02:01:38 -0500

    Better formated design code for GUI elements. Added Browse button for selection BT devices. Left some commented code for connecting to device with present PIN.

--- vConnect/Form1.Designer.cs
+++ vConnect/Form1.Designer.cs
@@ -31,25 +31,27 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.help_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.apply_button = new System.Windows.Forms.Button();
+            this.ok_button = new System.Windows.Forms.Button();
             this.BT_Devices = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.port_number = new System.Windows.Forms.Label();
+            this.server_IP = new System.Windows.Forms.Label();
+            this.edit_port = new System.Windows.Forms.Button();
+            this.edit_IP = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.label9 = new System.Windows.Forms.Label();
+            this.server_status_label = new System.Windows.Forms.Label();
+            this.server_test = new System.Windows.Forms.Button();
+            this.BT_ID = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.device_Status_Label = new System.Windows.Forms.Label();
+            this.refresh_button = new System.Windows.Forms.Button();
+            this.browse_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // trayIcon
@@ -59,56 +61,59 @@
             this.trayIcon.Text = "vConnect";
             this.trayIcon.Visible = true;
             // 
-            // button1
+            // help_button
             // 
-            this.button1.Location = new System.Drawing.Point(456, 299);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Help";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.help_button.Location = new System.Drawing.Point(456, 299);
+            this.help_button.Name = "help_button";
+            this.help_button.Size = new System.Drawing.Size(75, 23);
+            this.help_button.TabIndex = 0;
+            this.help_button.Text = "Help";
+            this.help_button.UseVisualStyleBackColor = true;
+            this.help_button.Click += new System.EventHandler(this.help_button_Click);
             // 
-            // button2
+            // cancel_button
             // 
-            this.button2.Location = new System.Drawing.Point(375, 299);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.cancel_button.Location = new System.Drawing.Point(375, 299);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 1;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
             // 
-            // button3
+            // apply_button
             // 
-            this.button3.Location = new System.Drawing.Point(294, 299);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Apply";
-            this.button3.UseVisualStyleBackColor = true;
+            this.apply_button.Location = new System.Drawing.Point(294, 299);
+            this.apply_button.Name = "apply_button";
+            this.apply_button.Size = new System.Drawing.Size(75, 23);
+            this.apply_button.TabIndex = 2;
+            this.apply_button.Text = "Apply";
+            this.apply_button.UseVisualStyleBackColor = true;
+            this.apply_button.Click += new System.EventHandler(this.apply_button_Click);
             // 
-            // button4
+            // ok_button
             // 
-            this.button4.Location = new System.Drawing.Point(213, 299);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Ok";
-            this.button4.UseVisualStyleBackColor = true;
+            this.ok_button.Location = new System.Drawing.Point(213, 299);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 3;
+            this.ok_button.Text = "Ok";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
             // 
             // BT_Devices
             // 
             this.BT_Devices.FormattingEnabled = true;
-            this.BT_Devices.Location = new System.Drawing.Point(12, 189);
+            this.BT_Devices.Location = new System.Drawing.Point(12, 198);
             this.BT_Devices.Name = "BT_Devices";
             this.BT_Devices.Size = new System.Drawing.Size(120, 95);
             this.BT_Devices.TabIndex = 4;
+            this.BT_Devices.SelectedIndexChanged += new System.EventHandler(this.BT_Devices_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 173);
+            this.label1.Location = new System.Drawing.Point(12, 182);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 5;
@@ -136,52 +141,52 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(213, 111);
+            this.label4.Location = new System.Drawing.Point(213, 110);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "Server Status:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // label5
+            // port_number
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(375, 45);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(103, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Current Port Number";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.port_number.AutoSize = true;
+            this.port_number.Location = new System.Drawing.Point(375, 45);
+            this.port_number.Name = "port_number";
+            this.port_number.Size = new System.Drawing.Size(103, 13);
+            this.port_number.TabIndex = 12;
+            this.port_number.Text = "Current Port Number";
+            this.port_number.Click += new System.EventHandler(this.label5_Click);
             // 
-            // label6
+            // server_IP
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(213, 45);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(129, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Current Server IP Address";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
+            this.server_IP.AutoSize = true;
+            this.server_IP.Location = new System.Drawing.Point(213, 45);
+            this.server_IP.Name = "server_IP";
+            this.server_IP.Size = new System.Drawing.Size(129, 13);
+            this.server_IP.TabIndex = 13;
+            this.server_IP.Text = "Current Server IP Address";
+            this.server_IP.Click += new System.EventHandler(this.label6_Click);
             // 
-            // button5
+            // edit_port
             // 
-            this.button5.Location = new System.Drawing.Point(378, 61);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "Edit ";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.edit_port.Location = new System.Drawing.Point(378, 61);
+            this.edit_port.Name = "edit_port";
+            this.edit_port.Size = new System.Drawing.Size(75, 23);
+            this.edit_port.TabIndex = 14;
+            this.edit_port.Text = "Edit ";
+            this.edit_port.UseVisualStyleBackColor = true;
+            this.edit_port.Click += new System.EventHandler(this.edit_port_Click);
             // 
-            // button6
+            // edit_IP
             // 
-            this.button6.Location = new System.Drawing.Point(213, 61);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 15;
-            this.button6.Text = "Edit";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.edit_IP.Location = new System.Drawing.Point(213, 61);
+            this.edit_IP.Name = "edit_IP";
+            this.edit_IP.Size = new System.Drawing.Size(75, 23);
+            this.edit_IP.TabIndex = 15;
+            this.edit_IP.Text = "Edit";
+            this.edit_IP.UseVisualStyleBackColor = true;
+            this.edit_IP.Click += new System.EventHandler(this.edit_IP_Click);
             // 
             // label7
             // 
@@ -192,33 +197,34 @@
             this.label7.TabIndex = 16;
             this.label7.Text = "BT Device ID:";
             // 
-            // label8
+            // server_status_label
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(213, 124);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(130, 13);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "Connected/Disconnected";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
+            this.server_status_label.AutoSize = true;
+            this.server_status_label.Location = new System.Drawing.Point(210, 123);
+            this.server_status_label.Name = "server_status_label";
+            this.server_status_label.Size = new System.Drawing.Size(130, 13);
+            this.server_status_label.TabIndex = 17;
+            this.server_status_label.Text = "Connected/Disconnected";
+            this.server_status_label.Click += new System.EventHandler(this.label8_Click);
             // 
-            // button7
+            // server_test
             // 
-            this.button7.Location = new System.Drawing.Point(212, 140);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 18;
-            this.button7.Text = "Test";
-            this.button7.UseVisualStyleBackColor = true;
+            this.server_test.Location = new System.Drawing.Point(213, 139);
+            this.server_test.Name = "server_test";
+            this.server_test.Size = new System.Drawing.Size(75, 23);
+            this.server_test.TabIndex = 18;
+            this.server_test.Text = "Test";
+            this.server_test.UseVisualStyleBackColor = true;
+            this.server_test.Click += new System.EventHandler(this.server_test_Click);
             // 
-            // label9
+            // BT_ID
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 49);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(18, 13);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "ID";
+            this.BT_ID.AutoSize = true;
+            this.BT_ID.Location = new System.Drawing.Point(12, 45);
+            this.BT_ID.Name = "BT_ID";
+            this.BT_ID.Size = new System.Drawing.Size(18, 13);
+            this.BT_ID.TabIndex = 19;
+            this.BT_ID.Text = "ID";
             // 
             // label10
             // 
@@ -229,39 +235,61 @@
             this.label10.TabIndex = 20;
             this.label10.Text = "Device Status:";
             // 
-            // label11
+            // device_Status_Label
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(18, 127);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(130, 13);
-            this.label11.TabIndex = 21;
-            this.label11.Text = "Connected/Disconnected";
+            this.device_Status_Label.AutoSize = true;
+            this.device_Status_Label.Location = new System.Drawing.Point(12, 123);
+            this.device_Status_Label.Name = "device_Status_Label";
+            this.device_Status_Label.Size = new System.Drawing.Size(130, 13);
+            this.device_Status_Label.TabIndex = 21;
+            this.device_Status_Label.Text = "Connected/Disconnected";
+            // 
+            // refresh_button
+            // 
+            this.refresh_button.Location = new System.Drawing.Point(12, 299);
+            this.refresh_button.Name = "refresh_button";
+            this.refresh_button.Size = new System.Drawing.Size(75, 23);
+            this.refresh_button.TabIndex = 22;
+            this.refresh_button.Text = "Refresh";
+            this.refresh_button.UseVisualStyleBackColor = true;
+            this.refresh_button.Click += new System.EventHandler(this.refresh_button_Click);
+            // 
+            // browse_button
+            // 
+            this.browse_button.Location = new System.Drawing.Point(138, 198);
+            this.browse_button.Name = "browse_button";
+            this.browse_button.Size = new System.Drawing.Size(75, 23);
+            this.browse_button.TabIndex = 23;
+            this.browse_button.Text = "Browse";
+            this.browse_button.UseVisualStyleBackColor = true;
+            this.browse_button.Click += new System.EventHandler(this.browse_button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 334);
-            this.Controls.Add(this.label11);
+            this.Controls.Add(this.browse_button);
+            this.Controls.Add(this.refresh_button);
+            this.Controls.Add(this.device_Status_Label);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.BT_ID);
+            this.Controls.Add(this.server_test);
+            this.Controls.Add(this.server_status_label);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edit_IP);
+            this.Controls.Add(this.edit_port);
+            this.Controls.Add(this.server_IP);
+            this.Controls.Add(this.port_number);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.BT_Devices);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.apply_button);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.help_button);
             this.Name = "Form1";
             this.Text = "vConnect";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -273,25 +301,27 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon trayIcon;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button help_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Button apply_button;
+        private System.Windows.Forms.Button ok_button;
         private System.Windows.Forms.ListBox BT_Devices;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label port_number;
+        private System.Windows.Forms.Label server_IP;
+        private System.Windows.Forms.Button edit_port;
+        private System.Windows.Forms.Button edit_IP;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label server_status_label;
+        private System.Windows.Forms.Button server_test;
+        private System.Windows.Forms.Label BT_ID;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label device_Status_Label;
+        private System.Windows.Forms.Button refresh_button;
+        private System.Windows.Forms.Button browse_button;
 
     }
 }
--- vConnect/Form1.cs
+++ vConnect/Form1.cs
@@ -7,17 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Sockets;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Windows.Forms;
 
 namespace vConnect
 {
+
+    
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+           
+           
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void cancel_button_Click(object sender, EventArgs e)
         {
 
         }
@@ -37,7 +47,7 @@ namespace vConnect
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void help_button_Click(object sender, EventArgs e)
         {
 
         }
@@ -57,18 +67,79 @@ namespace vConnect
 
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void edit_port_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void edit_IP_Click(object sender, EventArgs e)
         {
 
         }
 
         private void label8_Click(object sender, EventArgs e)
         {
+
+        }
+
+
+        // Refresh button for BT Devices
+        private void refresh_button_Click(object sender, EventArgs e)
+        {
+            var cli = new BluetoothClient();
+            BluetoothDeviceInfo[] peers = cli.DiscoverDevices();
+            this.BT_Devices.DisplayMember = "DeviceName";
+            this.BT_Devices.ValueMember = null;
+            this.BT_Devices.DataSource = peers;
+        }
+
+        private void BT_Devices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void apply_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void server_test_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browse_button_Click(object sender, EventArgs e)
+        {
+            var dlg = new SelectBluetoothDeviceDialog();
+            DialogResult result = dlg.ShowDialog(this);
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+            BluetoothDeviceInfo device = dlg.SelectedDevice;
+            BluetoothAddress addr = device.DeviceAddress;
+            /*    // bool isPaired = false;
+           var serviceClass = new Guid();
+           serviceClass = BluetoothService.SerialPort;
+           var cli = new BluetoothClient();
+           var ep = new BluetoothEndPoint(addr, serviceClass);
+         // isPaired = BluetoothSecurity.PairRequest(addr, "0000");
+           cli.Connect(ep);
+       if (cli.Connected)
+       {
+            
+           BluetoothDeviceInfo[] peers = cli.DiscoverDevices();
+           this.BT_Devices.DisplayMember = "DeviceName";
+           this.BT_Devices.ValueMember = null;
+           this.BT_Devices.DataSource = peers;
+
+       }*/
+
 
         }
     }
