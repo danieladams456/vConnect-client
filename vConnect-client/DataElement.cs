d48a0c52d129edee4f113942f08d9fd402e13844
Charlie <cesnyder217@gmail.com>
2015-02-02 23:26:37 -0500

    Added .gitignore

---
+++ vConnect-client/DataElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vConnect
+{
+    class DataElement
+    {
+        /// <summary>
+        /// This class describes a data element received from the car via BT.
+        /// </summary>
+        private string obdPID = "";
+        private int returnDataSize = 0;
+        private double returnData = 0;
+
+        public bool RequestDataFromCar()
+        {
+            return true;
+        }
+
+        public void FormatData()
+        {
+            return;
+        }
+
+        public bool SendEmergencyCode()
+        {
+            return true;
+        }
+    }
+}
