69e8fa3049e2d5d1e31279e898d3ee58224e3856
Charlie <cesnyder217@gmail.com>
2015-02-02 23:40:38 -0500

    Initial project creation and push. Take two.

---
+++ vConnect/BluetoothConnectionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vConnect
+{
+    /// <summary>
+    /// This class manages the BT connection.
+    /// </summary>
+    class BluetoothConnectionHandler
+    {
+        private string deviceID = "";
+        private bool BTConnectionStatus = false;
+        private string erorrMessageToUI = "";
+
+        bool EstablishBTConnection()
+        {
+            return true;
+        }
+
+        bool CloseBTConnection()
+        {
+            return true;
+        }
+
+        bool CheckBTConnection()
+        {
+            return BTConnectionStatus;
+        }
+
+        bool SendWindowsErrorMessage()
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ vConnect/DataCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace vConnect
+{
+    /// <summary>
+    /// This class holds and sends the data elements after they are
+    ///  cached for sending.
+    /// </summary>
+    class DataCache
+    {
+        private XmlDocument cache = new XmlDocument();
+
+        bool AddElementToCache()
+        {
+            return true;
+        }
+
+        bool SendToServer()
+        {
+            return true;
+        }
+
+        bool WriteToDisk()
+        {
+            return true;
+        }
+    }
+}
