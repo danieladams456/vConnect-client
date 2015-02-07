49a6b8fd54ee90b679728210fc7cad32342ee7c4
Charlie <cesnyder217@gmail.com>
2015-02-07 13:11:31 -0500

    Added some requestDataFromCar code as well as added a launch point in Form1 for testing purposes.

--- vConnect/DataElement.cs
+++ vConnect/DataElement.cs
@@ -12,13 +12,76 @@ namespace vConnect
         /// This class describes a data element received from the car via BT.
         /// </summary>
         private string obdPID = "";
+        private string name = "";
         private int returnDataSize = 0;
-        private double returnData = 0;
+        private byte[] returnData;
+
+        // This connection gets passed from the caller. It is the current connection.
+        BluetoothConnectionHandler BTConnection;
+
+        // This defines the largest potential size of return data from the car. It should never
+        //  really even get close to this value
+        private int MAX_DATA_SIZE= 20;
+
+        /// <summary>
+        /// Constructor that defines name and PID and number of bytes explicitly 
+        /// </summary>
+        /// <param name="elementName">name of the element to be collected</param>
+        /// <param name="PID">OBD PID to send to the car to get this value</param>
+        /// <param name="numberBytesReturned">Number of bytes the car will return</param>
+        /// <param name="btconnection">Current BT ConnectionHandler object.</param>
+        public DataElement(string elementName, string PID, int numberBytesReturned, BluetoothConnectionHandler btconnection)
+        {
+            name = elementName;
+            obdPID = PID;
+            returnData = new byte[numberBytesReturned];
+            BTConnection = btconnection;
+        }
+
+        /// <summary>
+        /// Default constructor if no parameters passed
+        /// </summary>
+        public DataElement()
+        {
+            returnData = new byte[MAX_DATA_SIZE];
+        }
 
         public bool RequestDataFromCar()
         {
+            /*
+            if (BT_Connection_Open)
+            {
+                //Craft the message to send to the car according to 
+			    // http://stackoverflow.com/questions/20477092/how-can-i-read-write-data-to-and-from-an-obd-ii-adapter-with-windows-phone-8
+			    string messageToSend = "01" + getobdPID() + "\r";
+			
+			    // Send the message to the car.
+			    //	For example, send "010D\r".
+			    WhateverBTCommandsAreNeededToPushDataToCar_Here(messageToSend);
+			
+			    // Since the data received back repeats the mode (e.g. Mode 01) and the PID (e.g. "0D"), we will have a 
+			    // 	temporary holding space for the response.
+			    // 	For example, the response will be like "410D17".
+			    int recievedRaw = 0;
+			    recievedRaw = listenForResponse();
+
+			    // Depending on the size of the actual return data we are looking for, shift the raw response the given
+			    // 	number of bytes and store it in returnData. We do not want the first two bytes (e.g. 410D)
+			    // 	See Maciek's answer on http://stackoverflow.com/questions/1318933/c-sharp-int-to-byte for more details.
+			    returnData = (byte)(recievedRaw >> 8*getReturnDataSize());	
+		
+		    }
+		    else 
+		    {
+    			AttemptToReestalblishBTConnection
+			    requestDataFromCar(obdPID,returnDataSize);
+		    }
+             * */
+
             return true;
+            
         }
+
 
         public void FormatData()
         {
@@ -31,6 +94,16 @@ namespace vConnect
         public bool SendEmergencyCode()
         {
             return true;
+        }
+
+        public string getobdPID()
+        {
+            return obdPID;
+        }
+
+        public int getReturnDataSize()
+        {
+            return returnDataSize;
         }
     }
 }
--- vConnect/Form1.cs
+++ vConnect/Form1.cs
@@ -14,22 +14,19 @@ using InTheHand.Net.Bluetooth;
 using InTheHand.Windows.Forms;
 
 namespace vConnect
-{
-
-    
+{    
 
     public partial class Form1 : Form
     {
-        BluetoothConnectionHandler BT_Connect = new BluetoothConnectionHandler();
-
-
-
+        BluetoothConnectionHandler BTConnection;
+  
         public Form1()
         {
+            BluetoothConnectionHandler BTConnection = new BluetoothConnectionHandler();
             InitializeComponent();
- 
-           
-           
+            
+            // This function begins requesting the car for data.
+            requestDataForElements();
         }
 
         private void cancel_button_Click(object sender, EventArgs e)
@@ -149,5 +146,47 @@ namespace vConnect
 
 
         }
+
+        public BluetoothConnectionHandler getBTConnection()
+        {
+            return BTConnection;
+        }
+
+        /// <summary>
+        /// This function serves as the launching point for requesting data.
+        ///     Note: These objects will not be explicitly named in the future.
+        /// </summary>
+        public void requestDataForElements()
+        {
+            // Obviously, for the final version, we will not explicitly name these and define
+            //  their obdPID and numBytesReturned values. THESE WILL COME FROM THE JSON SCHEMA.
+            //  However, for current testing purposes, it will be simpler to call each object by
+            //  the name of the actual element.
+            // Name, obdPID, numBytes
+            DataElement vin = new DataElement("vin", "02", 1, getBTConnection());
+            DataElement speed = new DataElement("speed", "0D", 1, getBTConnection());
+            DataElement rpm = new DataElement("rpm", "0C", 2, getBTConnection());
+            DataElement run_time_since_start = new DataElement("run_time_since_start", "1F", 2, getBTConnection());
+            DataElement fuel = new DataElement("fuel_level_input", "2F", 1, getBTConnection());
+            DataElement oil_temp = new DataElement("oil_temp", "5C", 1, getBTConnection());
+            DataElement accel = new DataElement("accel_position", "5A", 1, getBTConnection());
+            DataElement dist_with_MIL = new DataElement("distance_since_MIL", "21", 2, getBTConnection());
+
+            // Note that for the final version, we will be running all of this in some form of loop, 
+            //  not only one time. But for the purposes of making sure each part of this application is
+            //  functional, it will be simplest to call them explicitly, once.
+            vin.RequestDataFromCar();
+            speed.RequestDataFromCar();
+            rpm.RequestDataFromCar();
+            run_time_since_start.RequestDataFromCar();
+            fuel.RequestDataFromCar();
+            oil_temp.RequestDataFromCar();
+            accel.RequestDataFromCar();
+            dist_with_MIL.RequestDataFromCar();
+        }
+
     }
+
+
+
 }
